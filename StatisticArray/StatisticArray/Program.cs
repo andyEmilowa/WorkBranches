841b3bfde8743052e036b32920ef0dd769cedc10
andyEmilowa <andyowaa@gmail.com>
2023-03-02 11:22:23 +0200

    new method

---
+++ StatisticArray/StatisticArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StatisticArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("count =" + OptionsManager.CountLess100(arr));
+            Console.WriteLine($"Max ={OptionsManager.MaxNum(arr)}");
+            Console.WriteLine($"Min ={OptionsManager.MinNum(arr)}");
+            arr =OptionsManager.ReverseArr(arr);
+            OptionsManager.PrintArr(arr);
+            // TO DO
+        }
+    }
+}
